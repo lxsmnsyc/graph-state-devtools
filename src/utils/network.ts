68c6eb10e64e2731c1a83cd9ef0668a4ec2b72fa
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-11-13 21:38:36 +0800

    Fix camera movement

--- src/utils/network.ts
+++ src/utils/network.ts
@@ -4,6 +4,11 @@ export function moveNetworkViewToNode(
   instance: Network,
   id: string,
 ): void {
+  // Capture current position
+  instance.moveTo({
+    scale: instance.getScale(),
+    position: instance.getViewPosition(),
+  });
   instance.fit({
     nodes: [id],
     animation: true,
