22de623105bfd016eaafcc8610179a7e3b03c726
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-11-15 01:10:48 +0800

    Update devtools.js

--- devtools.js
+++ devtools.js
@@ -1,8 +1,15 @@
-chrome.devtools.inspectedWindow.eval(
-  'typeof withGraphStateDomainMemory',
-  (result) => {
-    if (result !== 'undefined') {
-      chrome.devtools.panels.create('Graph State DevTools', '', 'panel.html');
-    }
-  },
-);
+function init() {
+  chrome.devtools.inspectedWindow.eval(
+    'typeof withGraphStateDomainMemory',
+    (result) => {
+      if (result !== 'undefined') {
+        chrome.devtools.panels.create('Graph State DevTools', '', 'panel.html');
+      } else {
+        requestAnimationFrame(init);
+      }
+    },
+  );
+}
+
+init();
+
