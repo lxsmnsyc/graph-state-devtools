2fdb638a46ac289034163716572541d896fdbf77
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-11-15 01:10:43 +0800

    Update refresh-interval.ts

--- src/nodes/refresh-interval.ts
+++ src/nodes/refresh-interval.ts
@@ -2,13 +2,6 @@ import { createGraphNode } from 'graph-state';
 
 const refreshInterval = createGraphNode({
   get: 5000,
-  set: ({ mutateSelf }, value: number) => {
-    if (value > 100) {
-      mutateSelf(value);
-    } else {
-      mutateSelf(100);
-    }
-  },
 });
 
 export default refreshInterval;
