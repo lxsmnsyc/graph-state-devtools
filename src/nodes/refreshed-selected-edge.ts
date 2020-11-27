c9064606d7d7b869eb8dbf12d082a2c067e3446c
Alexis Munsayac <alexis.munsayac@gmail.com>
2020-11-27 15:27:56 +0800

    Remove refreshEffect

--- src/nodes/refreshed-selected-edge.ts
+++ src/nodes/refreshed-selected-edge.ts
@@ -3,7 +3,6 @@ import { createGraphNode } from 'graph-state';
 import edges, { DataEdge } from './edges';
 import networkSelectedEdge from './network-selected-edge';
 import refresh from './refresh';
-import refreshEffect from './refresh-effect';
 
 interface RefreshedEdge {
   edge?: DataEdge;
@@ -12,7 +11,6 @@ interface RefreshedEdge {
 const refreshedSelectedEdge = createGraphNode<RefreshedEdge>({
   get: ({ get }) => {
     get(refresh);
-    get(refreshEffect);
     const id = get(networkSelectedEdge);
     return {
       edge: id ? get(edges).get(id) ?? undefined : undefined,
--- src/nodes/refreshed-selected-node.ts
+++ src/nodes/refreshed-selected-node.ts
@@ -2,7 +2,6 @@ import { createGraphNode } from 'graph-state';
 
 import nodes, { DataNode } from './nodes';
 import refresh from './refresh';
-import refreshEffect from './refresh-effect';
 import networkSelectedNode from './network-selected-node';
 
 interface RefreshedSelectedNode {
@@ -12,7 +11,6 @@ interface RefreshedSelectedNode {
 const refreshedSelectedNode = createGraphNode<RefreshedSelectedNode>({
   get: ({ get }) => {
     get(refresh);
-    get(refreshEffect);
     const id = get(networkSelectedNode);
     return {
       node: id ? get(nodes).get(id) ?? undefined : undefined,
